65cccff44bd2c8cfd8a7d44555c204d5f20d4a73
yutzuliu <bb450277@gmail.com>
2018-11-20 14:24:52 +0100

    button scene

---
+++ Assets/SceneToNavigate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneToNavigate : MonoBehaviour {
+
+    string SeneToNavige;
+
+    public  void SceneToNavigateTo()
+    {
+        SceneManager.LoadScene(SeneToNavige);
+    }
+}
